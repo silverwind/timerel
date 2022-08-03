68b4b1a34369d56bbf2b646034fd0ff3780955f7
silverwind <me@silverwind.io>
2022-08-03 20:35:11 +0200

    add bench.js

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # timerel
 [![](https://img.shields.io/npm/v/timerel.svg?style=flat)](https://www.npmjs.org/package/timerel) [![](https://img.shields.io/npm/dm/timerel.svg)](https://www.npmjs.org/package/timerel) [![](https://img.shields.io/bundlephobia/minzip/timerel.svg)](https://bundlephobia.com/package/timerel)
 
-`timerel` formats dates to english relative format `5 mins ago` and does so around 9 times faster than [its competition](https://github.com/hustcc/timeago.js).
+`timerel` formats dates to english relative format `5 mins ago` and does so around [9 times faster](./bench.js) than [its competition](https://github.com/hustcc/timeago.js).
 
 ## Usage
 
---
+++ bench.js
@@ -0,0 +1,19 @@
+import {format} from "timeago.js";
+import {timerel} from "./index.js";
+
+// benchmark
+const now = Date.now();
+const t1 = performance.now();
+for (let i = 0; i < 1000000; i++) timerel(now - i * 100000);
+console.info(`timerel: ${Math.round(performance.now() - t1)}ms`);
+const t2 = performance.now();
+for (let i = 0; i < 1000000; i++) format(now - i * 100000);
+console.info(`timeago: ${Math.round(performance.now() - t2)}ms`);
+
+// verify correctness
+for (let i = 0; i < 1000000; i++) {
+  const v = now - i * 100000;
+  const a = timerel(v);
+  const b = format(v).replace("minute", "min").replace("second", "sec").replace("just ", "");
+  if (a !== b) console.info({a, b});
+}
