023fe16b339b026379f74d01ccefdbec9db88b51
silverwind <me@silverwind.io>
2022-11-10 23:19:07 +0100

    move test to tests

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 import {timerel} from "./index.js";
+import {format} from "timeago.js";
 
 test("test", () => {
   const date = Date.now();
@@ -9,5 +10,15 @@ test("test", () => {
     expect(timerel(new Date(date + i))).toMatch(/^(in [0-9]+ [a-z]+|now)/);
     expect(timerel(date - i, {noAffix: true})).toMatch(/^([0-9]+ [a-z]+|now)/);
     expect(timerel(date + i, {noAffix: true})).toMatch(/^([0-9]+ [a-z]+|now)/);
+  }
+});
+
+test("correctness", () => {
+  const now = Date.now();
+  for (let i = 0; i < 1000000; i++) {
+    const val = now - i * 100000;
+    const a = timerel(val);
+    const b = format(val).replace("minute", "min").replace("second", "sec").replace("just ", "");
+    expect(a).toEqual(b);
   }
 });
